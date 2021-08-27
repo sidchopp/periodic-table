f8ca5b90317c69f2b2b4f81539f8e8b52a8d5dd6
Sid <sidchopp@gmail.com>
2021-08-27 12:53:07 -0600

    fetched elemnts name displayed on ui using map method

---
+++ src/components/ElementCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ElementCard() {
+  return (
+    <div>
+      My Card
+    </div>
+  )
+}
+
+export default ElementCard
--- src/components/PeriodicTable.js
+++ src/components/PeriodicTable.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
+import { Grid, Image } from 'semantic-ui-react'
+
+
 // components
 import Loading from './Loading';
+//import ElementCard from './ElementCard';
 
 
 const url = 'https://neelpatel05.pythonanywhere.com/'
@@ -21,21 +25,47 @@ function PeriodicTable() {
       const data = await response.json();
       console.log(data);
       // after getting our data, we want loading to stop
-      setTable(false)
+      setLoading(false)
       // to show the data of tours
       setTable(data)
     } catch (err) {
       // if there is error we want to stop loading
       setLoading(false)
-      console.log(err);
+      console.log('This is the error:', err);
     }
   }
   useEffect(() => {
     fetchTable();
   }, [])
+
+  const mapTable = table.map((elements) => {
+    return (
+      <div>
+        <h1>
+          {elements.name}
+        </h1>
+      </div>
+
+    )
+  })
   return (
     <div>
-      My Periodic Table
+
+      <Grid>
+        {mapTable}
+      </Grid>
+      {/* {table.map((elements) => {
+        return (
+          <Grid key={elements.atomicNumber}>
+            <Grid.Row columns={10}>
+              <Grid.Column>
+                <Image src='/images/wireframe/image.png' />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        )
+      })} */}
+      {/* <ElementCard /> */}
     </div>
   )
 }
