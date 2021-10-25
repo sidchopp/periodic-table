6c07f49435020da625ee6e326867b9f6496a75f3
Sid <sidchopp@gmail.com>
2021-10-24 23:02:47 -0600

    Icon in nav is modified with a Popup

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { Menu, Icon, Header } from 'semantic-ui-react'
+import { Menu, Icon, Header, Popup } from 'semantic-ui-react'
 
 const Nav = () => (
   <div className='nav'>
     <Menu compact style={{ margin: '1vw' }} >
       <Menu.Item>
         <Header as='h2'>
-          <a href="https://sid-projects.netlify.app/" target="_blank" rel="noreferrer"> <Icon color='black' link name='sidebar' /></a>
-          <Header.Content style={{ fontSize: '1.8vw' }} >The Periodic Table</Header.Content>
+          <a href="https://sid-projects.netlify.app/" target="_blank" rel="noreferrer" >
+            <Popup
+              offset={[0, 18]}
+              inverted
+              position='bottom center'
+              content='About Me'
+              trigger={<Icon color='black' link name='user secret' />}
+            />
+          </a>
+          <Header.Content style={{ fontSize: '1.8vw' }} >
+            The Periodic Table
+          </Header.Content>
         </Header>
       </Menu.Item>
     </Menu>
